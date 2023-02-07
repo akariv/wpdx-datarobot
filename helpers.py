55051f30451b31f8c366e47e63b28f22c6d7eccf
Adam Kariv <akariv@users.noreply.github.com>
2023-02-07 08:36:41 +0200

    Latest from chandler

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import datarobot as dr
+import pandas as pd
 
-def get_top_of_leaderboard(project, metric="AUC", feature_list=fl)# , verbose=True):
+def get_top_of_leaderboard(project,fl_name=None, metric="AUC"):# , verbose=True):
     """
     A helper method to assemble a dataframe with leaderboard results and print a summary.
     """
@@ -24,9 +25,15 @@ def get_top_of_leaderboard(project, metric="AUC", feature_list=fl)# , verbose=Tr
         "FVE Binomial",
     ]
     asc_flag = False if metric in desc_metric_list else True
+    
 
     leaderboard = []
-    for m in project.get_models():
+    if fl_name: #enable filtering for certain feature lists
+        models=[m for m in project.get_models() if m.featurelist_name==fl_name]
+    else:
+        models = project.get_models()
+        
+    for m in models:
         leaderboard.append(
             [
                 m.blueprint_id,
@@ -35,7 +42,8 @@ def get_top_of_leaderboard(project, metric="AUC", feature_list=fl)# , verbose=Tr
                 m.model_type,
                 m.sample_pct,
                 m.metrics[metric]["validation"],
-                m.metrics[metric]["crossValidation"],
+                m.metrics[metric]["crossValidation"]
+                m.metrics[metric]["holdout"],
             ]
         )
     leaderboard_df = pd.DataFrame(
@@ -47,14 +55,35 @@ def get_top_of_leaderboard(project, metric="AUC", feature_list=fl)# , verbose=Tr
             "pct",
             f"validation_{metric}",
             f"cross_validation_{metric}",
+            f"holdout_{metric}",
         ],
         data=leaderboard,
     )
+#     leaderboard_top = (
+#         leaderboard_df[(leaderboard_df["pct"]<84)&(leaderboard_df['featurelist']==feature_list.name)] #only get non-frozen models from the final round of autopilot before deployments
+#         .sort_values(by=f"cross_validation_{metric}", ascending=asc_flag)
+#         .head()
+#         .reset_index(drop=True)
+#     )
     leaderboard_top = (
-        leaderboard_df[(leaderboard_df["pct"]<84)&(leaderboard_df['featurelist'==fl])] #only get non-frozen models from the final round of autopilot before deployments
+        leaderboard_df[round(leaderboard_df["pct"]) <84] #don't use frozen models
         .sort_values(by=f"cross_validation_{metric}", ascending=asc_flag)
         .head()
         .reset_index(drop=True)
     )
     
     return leaderboard_top
+
+def make_cv_data(df, country):
+    frame = df.loc[df.clean_country_id==country]
+
+    holdout_rows= round(frame.shape[0]*.15)
+    #holdout_fake_date = '1999-01-01'
+    print(holdout_rows)
+    holdout = frame.sort_values(by='report_date', ascending=False).head(holdout_rows)
+    
+    indices = holdout.index
+    #ug.loc[holdout.index,:]['fold'] = '1999-01-01'
+    frame.loc[indices,'fold'] = 'fold_-1'
+    #frame['fold_yr']= pd.to_datetime(frame['fold']).apply(lambda x: x.year)
+    return frame
